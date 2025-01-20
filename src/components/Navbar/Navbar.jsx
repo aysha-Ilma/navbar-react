6278b39f008267399a13583f0af08282aab6e2d9
aysha-ilma <mmayshailma@gmail.com>
2025-01-20 20:03:53 +0530

    coding Light & Dark Mode Navigation Bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,11 +7,17 @@ import search_icon_dark from '../../assets/search-b.png'
 import toogle_light from '../../assets/night.png'
 import toogle_dark from '../../assets/day.png'
 
-const Navbar = () => {
+const Navbar = ({theme, setTheme}) => {
+
+    const toggle_mode = ()=>{
+        theme == 'light' ? setTheme ('dark') : setTheme('light');
+         
+    }
+
   return (
     <div className='navbar'>
 
-        <img src={logo_light} alt="" className='logo'/>
+        <img src={theme == 'light' ? logo_light : logo_dark} alt="" className='logo'/>
 
         <ul>
             <li>Home</li>
@@ -22,9 +28,10 @@ const Navbar = () => {
 
         <div className='search-box'>
             <input type="text" placeholder='Search'/>
-            <img src={search_icon_light} alt="" />
+            <img src={theme == 'light' ? search_icon_light : search_icon_dark} alt="" />
         </div>
-        <img src={toogle_light} alt='' className='toogle_icon'/>
+
+        <img onClick={()=> {toggle_mode()}} src={theme == 'light' ? toogle_light : toogle_dark} alt='' className='toogle_icon'/>
         
     </div>
   )
